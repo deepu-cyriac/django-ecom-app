c7346448e12a1709d5786eeebcfdf6423b2ebf16
Deepu Cyriac <deepucvk123@gmail.com>
2024-02-17 19:36:35 +0530

    Initialised Homepage app in Django

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("polls/", include("polls.urls")),
+    path('', include('homepage.urls')),
+    path('polls/', include('polls.urls')),
 ]
